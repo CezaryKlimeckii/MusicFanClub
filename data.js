1dc95ca19afa9e42699f4611fe68b72141b36cd1
cezaryklimeckistrony <cezaryklimeckistrony@gmail.com>
2024-05-03 20:44:29 +0200

    first

---
+++ data.js
@@ -0,0 +1,51 @@
+let songs = [
+    {
+        name: '"Should I Stay or Should I Go"',
+        path: 'images/The Clash - Should I Stay or Should I Go (Official Audio) (1).mp3',
+        artist: 'The Clash',
+        cover: 'images/The_Clash_-_Combat_Rock.jpg'
+    },
+    {
+        name: '"Scar Tissue"',
+        path: 'images/Red Hot Chili Peppers - Scar Tissue (Lyrics) (1).mp3',
+        artist: 'Red Hot Chilli Peppers',
+        cover: 'images/Red hot chilli peppers - Scar Tissue.jpg'
+    },
+    {
+        name: '"So Doggone Lonesome"',
+        path: 'images/So Doggone Lonesome (Remastered 2022) (1).mp3',
+        artist: 'Johny Cash',
+        cover: 'images/So Doggone Lonesome Alboum Cover.jpg'
+    },
+    {
+        name: '"Bodyguard"',
+        path: 'images/Beyoncé - BODYGUARD (1).mp3',
+        artist: 'Beyoncé',
+        cover: 'images/Beyonce-Cowboy-Carter.webp'
+    },
+    {
+        name: '"Mulberry Street"',
+        path: 'images/Mulberry Street - Twenty One Pilots - Official Audio (1).mp3',
+        artist: 'Twenty One Pilots',
+        cover: 'images/twenty one pilots album ocver.jpg'
+    },
+    {
+        name: '"I Dont Trust Myself (With Loving You)"',
+        path: 'images/I Dont Trust Myself (With Loving You) (1).mp3',
+        artist: 'John Mayer',
+        cover: 'images/john mayer album cover continuum.jpg'
+    },
+    {
+        name: '"If You Want Me To Stay"',
+        path: 'images/Sly & The Family Stone - If You Want Me To Stay (Audio) (1).mp3',
+        artist: 'Sly & The Family Stone',
+        cover: 'images/Slyfamstone-fresh.jpg'
+    },
+    {
+        name: '"Resonance"',
+        path: 'images/HOME - Resonance (1).mp3',
+        artist: 'HOME',
+        cover: 'images/home resonance album cover.jpg'
+    }
+
+]
